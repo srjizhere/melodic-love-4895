19b02dc54c5c2625fa20b483cfc395f154890b54
srjizhere <bhiapatil@gmail.com>
2022-12-16 18:52:44 +0530

    footerinsert

---
+++ Front-end/components/foot/footer.js
@@ -0,0 +1,81 @@
+ export default function footer(){
+    return ` 
+    <div class="foot">
+    <div class="foot-column">
+       <ul>
+           <li><label class="head">Customer Service</label></li>
+           <li>Contact Us</li>
+           <li>Order Status</li>
+           <li>Shipping</li>
+           <li>Returns &amp;exchanges</li>
+           <li>Shipping</li>
+           <li>Product recalls</li>
+       </ul>
+    </div>
+    <div class="foot-column">
+          <ul>
+              <li><label class="head">
+                  Stores &amp; Events
+              </label>
+              </li>
+              <li> Stores &amp; Events</li>
+              <li>Carrers</li>
+              <li>Corporate Resposibility</li>
+              <li>Diversity Including &amp; Belonging</li>
+              <li>Get Email Updates</li>
+              <li>Nordstrom Blog</li>
+          </ul>
+    </div>
+    <div class="foot-column">
+      <ul>
+          <li><label class="head">Service</label></li>
+          <li>Personal Styling</li>
+          <li>Alteration &amp; Tailering</li>
+          <li>Spa Nordstrom</li>
+          <li>Nordstrom Restuarant</li>
+          <li> Gift Cards</li>
+      </ul>
+    </div>
+    <div class="foot-column">
+      <ul>
+          <li><label class="head">Nordstrom Cards</label></li>
+          <li>Pay My Bill</li>
+          <li>Manage Cards</li>
+      </ul>
+    </div>
+    <div class="foot-column">
+     <ul>
+         <li><label class="head">Nordstrom Inc</label></li>
+         <li>Nordstrom Rack</li>
+         <li>Nordstrom Canada</li>
+         <li>Trunk Club</li>
+         <li>Investor Relations</li>
+         <li>Press Release</li>
+     </ul>
+    </div>
+  
+  <div id="media">
+    <div id="mobpic">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScI5kA6BW-71n2xL3eh5ffUzBB1I2cOfER2ts6rOhws04TX_hsqAwAJTpB4EjkjK1rqgQ&usqp=CAU" alt=""><h3>Get our apps</h3>
+    </div>
+    <div id="srjimgfooterlast">
+        <div class="srjimgmedialast"><img src="https://cdn.modesens.com/static/img/20220726Pinterest.svg" alt=""></div>
+        <div class="srjimgmedialast"><img src="https://cdn.modesens.com/static/img/20220726Twitter.svg" alt=""></div>
+        <div class="srjimgmedialast"><img src="https://cdn.modesens.com/static/img/20220726Ins.svg" alt=""></div>
+        <div class="srjimgmedialast"><img src="https://cdn.modesens.com/static/img/20220726Facebook.svg" alt=""></div>
+        <div class="srjimgmedialast"><img src="https://cdn.modesens.com/static/img/20220726Weibo.svg" alt=""></div>
+    </div>
+  </div>
+</div>
+<div id="foot-second">
+    <p>Your Privacy Rights</p>
+    <p>Dont Sell My Info</p>
+    <p>Terms &amp; Conditions</p>
+    <p>Interest Based Ads</p>
+    <p>&copy; 2021 Nordstrom, Inc</p>
+</div>
+`
+    
+}
+
+// export   {footer}
